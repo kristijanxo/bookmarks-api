8141287a491d970c0a60db1304664e1494885002
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2026-08-28 21:25:16 +0200

    chore: add initial module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module.js';
+import { UserModule } from './user/user.module.js';
+import { BookmarkModule } from './bookmark/bookmark.module.js';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
