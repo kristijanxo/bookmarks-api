64b5eac65bf843a2ad1068cab99267d9172890e8
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2026-09-10 13:56:49 +0200

    chore: set up global ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module.js';
 import { UserModule } from './user/user.module.js';
 import { BookmarkModule } from './bookmark/bookmark.module.js';
 import { DatabaseModule } from './database/database.module.js';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
