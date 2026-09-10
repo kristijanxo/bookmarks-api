81e5e779fbbab1670418cfe6d5927d500f67ca92
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2026-09-10 11:43:38 +0200

    feat(auth): implement basic sign-in logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
   }
 
   @Post('sign-in')
-  signIn() {
-    return this.authService.signIn();
+  signIn(@Body() dto: AuthDto) {
+    return this.authService.signIn(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import * as argon from 'argon2';
 import { DatabaseService } from '../database/database.service.js';
 import { AuthDto } from './dto/index.js';
@@ -18,21 +22,46 @@ export class AuthService {
 
     const hash = await argon.hash(dto.password);
 
-    const user = await this.database.db.orm.public.User.select(
-      'id',
-      'firstName',
-      'lastName',
-      'email',
-      'createdAt',
-    ).create({
+    const user = await this.database.db.orm.public.User.create({
       email: dto.email,
       hash,
     });
 
-    return user;
+    return {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      createdAt: user.createdAt,
+    };
   }
 
-  signIn() {
-    return { msg: 'I have signed in!' };
+  async signIn(dto: AuthDto) {
+    //find user by email
+    //if user does not exist throw exception
+    const user = await this.database.db.orm.public.User.where({
+      email: dto.email,
+    }).first();
+
+    if (!user) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    //compare password
+    //if passord false trhow exception
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    //send back user
+    return {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      createdAt: user.createdAt,
+    };
   }
 }
